89984b6e824dafd634726825af6cf2f0c46d40dd
Nikita Dudnik (nek) <nikdudnik@gmail.com>
2024-09-15 13:32:17 +0200

    Microfixes

--- src/cender/scratch.clj
+++ src/cender/scratch.clj
@@ -7,24 +7,24 @@
   (println (call-fn "move_object" ["Cube" (Vector. [0 1 0])])))
 
 
+;; Custom types support is easily implementable in this version!
 ;; TODO:
-;; [ ] Color,
+;; [x] Vector,
+;; [x] Color,
 ;; [ ] Euler,
 ;; [ ] Matrix,
-;; [ ] Quaternion,
-;; [x] Vector,
+;; [ ] Quaternion.
 
 (connect-client "tcp://127.0.0.1:10000")
-;; (println (eval "a = 100"))
-;; (println "Send:" (set-var "a" 100))
-;; (println "Receive:" (get-var "a"))
-;; (call-fn "move_object" ["Cube" (Vector. [0 1 0])])
-;; (set-obj-prop "Cube" "location" (Vector. [-1 2 0]))
 
+(println "Send:" (set-var "a" 100))
+(println "Receive:" (get-var "a"))
+(call-fn "move_object" ["Cube" (Vector. [0 1 0])])
+(set-obj-prop "Cube" "location" (Vector. [-1 2 0]))
 (println (get-obj-prop "Cube" "location"))
+(set-item-prop "materials" "Material" "diffuse_color" [1 0 0 1])
 
-;; (def col (get-item-prop "materials" "Material" "specular_color"))
-;; col
-(set-item-prop "materials" "Material" "diffuse_color" [ 1 0 0 1])
-
-(add-cube "ShinyCube" 1)
+;; This doesn't work as I intended it to because we are using quite a hackish way of controlling Blender.
+;; We are modifying objects from a background thread. This is huge DON'T DO IT in the manual.
+;; The next step is to unhack it. 
+#_(add-cube "ShinyCube" 1)

1c762964813e5b9ee3bbc4ff3a99c7451123e93a
Nikita Dudnik (nek) <nikdudnik@gmail.com>
2024-09-15 12:54:41 +0200

    Dump experiment

--- src/cender/scratch.clj
+++ src/cender/scratch.clj
@@ -1,6 +1,6 @@
 (ns cender.scratch
-  (:require [cender.v1.client :refer [connect-client call-fn eval-code set-var get-var import-ns set-obj-prop]])
-  (:import [cender.v1.client Vector]))
+  (:require [cender.v1.client :refer [connect-client call-fn eval-code set-var get-var import-ns set-obj-prop get-obj-prop set-item-prop get-item-prop add-cube]])
+  (:import [cender.v1.client Vector Color]))
 
 #_(defn -main [& args]
   (connect-client "tcp://127.0.0.1:10000")
@@ -19,5 +19,12 @@
 ;; (println "Send:" (set-var "a" 100))
 ;; (println "Receive:" (get-var "a"))
 ;; (call-fn "move_object" ["Cube" (Vector. [0 1 0])])
-(set-obj-prop "Cube" "location" (Vector. [1 1 1]))
+;; (set-obj-prop "Cube" "location" (Vector. [-1 2 0]))
 
+(println (get-obj-prop "Cube" "location"))
+
+;; (def col (get-item-prop "materials" "Material" "specular_color"))
+;; col
+(set-item-prop "materials" "Material" "diffuse_color" [ 1 0 0 1])
+
+(add-cube "ShinyCube" 1)
--- src/cender/v1/client.clj
+++ src/cender/v1/client.clj
@@ -4,6 +4,12 @@
   (:require [cognitect.transit :as transit]))
 
 (defrecord Vector [vals])
+(defrecord Color [r g b])
+
+(def writers {Vector (transit/write-handler "blender-vector" (fn [o] (:vals o)))
+              Color (transit/write-handler "blender-color" (fn [{:keys [r g b]}] [r g b]))})
+(def readers {"blender-vector" (transit/read-handler (fn [v] (Vector. v)))
+              "blender-color" (transit/read-handler (fn [[r g b]] (Color. r g b)))})
 
 (defprotocol Connector
   "Something that can connect to a server."
@@ -29,20 +35,23 @@
   Caller
   (call [_ args]
     (let [out (ByteArrayOutputStream. 4096)
-          writer (transit/writer out :json {:handlers {Vector (transit/write-handler "blender-vector" (fn [o] (:vals o)))}})]
+          writer (transit/writer out :json {:handlers writers})]
       (transit/write writer args)
       (let [res (.call client "call_fn" (object-array [(.toString out)]))
             in (ByteArrayInputStream. (.getBytes res))
-            reader (transit/reader in :json {:handlers {"blender-vector" (transit/read-handler (fn [v] (Vector. v)))}})]
+            reader (transit/reader in :json {:handlers readers})]
         (transit/read reader)))))
 
 (def client (atom nil))
 
 (defn connect-client [addr]
   (let [cl (->RpcClient (ZRpcClient.) addr)]
-    (.connect cl)
     (reset! client cl)
+    (.connect cl)
     cl))
+
+(defn disconnect []
+  (.disconnect @client))
 
 (defn call-fn [name args]
   (.call @client (cons name args)))
@@ -64,3 +73,17 @@
 
 (defn set-obj-prop [obj prop val]
   (call-fn "set_obj_prop" [obj prop val]))
+
+(defn get-obj-prop [obj prop]
+  (call-fn "get_obj_prop" [obj prop]))
+
+(defn set-item-prop [coll-name item-name prop-name val]
+  (call-fn "set_item_prop" [coll-name item-name prop-name val]))
+
+(defn get-item-prop [coll-name item-name prop-name]
+  (call-fn "get_item_prop" [coll-name item-name prop-name]))
+
+
+(defn add-cube [name size]
+  (call-fn "add_cube" [name size]))
+
